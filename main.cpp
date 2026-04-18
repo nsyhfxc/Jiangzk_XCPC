fc244ce9de90338f62d6e0052ba3279a1e73d4af
念碎夜画繁星辰 <1691034079@qq.com>
2026-04-18 20:09:35 +0800

    INIT

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
-#include <iostream>
+#include<bits/stdc++.h>
+#define endl '\n'
+using namespace std;
+using i64 = long long;
+using u64 = unsigned long long;
+
+void solve() {
+    cout << "My new start" << endl;
+    /*
+    因为撞了南墙,所以现在才知道回头.
+    2026年4月18日.
+    */
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
     return 0;
 }
