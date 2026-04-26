5af5f6f980430d2b8ede3de1080c3d740bdf1b0a
念碎夜画繁星辰 <1691034079@qq.com>
2026-04-26 19:43:39 +0800

    Merge remote-tracking branch 'origin/main' 推送 # Conflicts: #	.idea/workspace.xml #	main.cpp

---
+++ CodeForces/Educational Codeforces Round 189/A.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+using namespace std;
+using i64 = long long;
+using u64 = unsigned long long;
+
+void solve() {
+    i64 x, y;
+    cin >> x >> y;
+
+    auto check = [&](i64 q) {
+        return q * x < y;
+    };
+
+    i64 l = 1, r = y / x;
+    i64 q = 1;
+
+    while (l <= r) {
+        const i64 mid = (l + r) / 2;
+        if (check(mid)) {
+            q = mid;
+            l = mid + 1;
+        } else {
+            r = mid - 1;
+        }
+    }
+    i64 z = q * x;
+    if (z > x && z < y && y % z != 0) {
+        cout << "YES" << endl;
+    } else cout << "NO" << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+}
---
+++ CodeForces/Educational Codeforces Round 189/B.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+using namespace std;
+using i64 = long long;
+using u64 = unsigned long long;
+
+void solve() {
+    string s;
+    cin >> s;
+    auto n = s.size();
+    vector<int> mis(n);
+    for (int i = 0; i < n; i++) {
+        char c = (i % 2 == 0 ? 'a' : 'b');
+        mis[i] = (s[i] != c);
+    }
+    auto calc = [&](int x) {
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            if (mis[i] == x && (i == 0 || mis[i - 1] != x)) {
+                cnt++;
+            }
+        }
+        return cnt;
+    };
+    if (calc(0) <= 1 || calc(1) <= 1) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+}
