f5ce5bcbe2aa61c74c1fc6d64404671980e0bf00
念碎夜画繁星辰 <1691034079@qq.com>
2026-05-08 19:36:34 +0800

    提交比赛代码

--- NowCoder/XiaoBai132/C.cpp
+++ NowCoder/XiaoBai132/C.cpp
@@ -1,39 +1,40 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #define endl '\n'
 using namespace std;
 using i64 = long long;
 using u64 = unsigned long long;
-const 
-void solve() {
-    i64 l, r;
-    cin >> l >> r;
-    auto ksm = [&](i64 a, i64 b, i64 MOD)-> i64 {
-        i64 res = 1;
-        a %= MOD;
-        while (b) {
-            if (b & 1) {
-                res = (res * a) % MOD;
-            }
-            b >>= 1;
-            a = (a * a) % MOD;
-        }
-        return res;
-    };
-    for (int i = 0; i <= 63; i++) {
-        for (int j = 0; j <= 63; j++) {
-            if (ksm(2,i,mod) + )
 
+void solve() {
+    vector<u64> arr;
+    for (int i = 0; i < 63; i++) {
+        for (int j = i; j < 63; j++) {
+            arr.push_back((1ULL << i) + (1ULL << j));
         }
+    }
+
+    sort(arr.begin(), arr.end());
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        u64 l, r;
+        cin >> l >> r;
+
+        auto left = lower_bound(arr.begin(), arr.end(), l);
+        auto right = upper_bound(arr.begin(), arr.end(), r);
+
+        cout << right - left << endl;
     }
 }
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+
     int t = 1;
-    cin >> t;
+    // cin >> t;
     while (t--) {
         solve();
     }
-    return 0;
 }
