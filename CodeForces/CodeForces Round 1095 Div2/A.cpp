6523ac848ea8e0ff6d87b55092728a7cf884d80b
念碎夜画繁星辰 <1691034079@qq.com>
2026-05-29 20:59:26 +0800

    123456

--- CodeForces/CodeForces Round 1095 Div2/A.cpp
+++ CodeForces/CodeForces Round 1095 Div2/A.cpp
@@ -5,16 +5,6 @@ using u64 = unsigned long long;
 #define endl '\n'
 
 void solve() {
-    int x, y;
-    cin >> x >> y;
-    // x是偶数，y是偶数 -> 肯定可以
-    // x 或 y 有一个是偶数 -> 肯定可以
-    // x 或 y 都是奇数 ->肯定不可以
-    bool ok = true;
-    if (x & 1 && y & 1) {
-        ok = false;
-    }
-    cout << (ok ? "YES" : "NO") << endl;
 }
 
 int main() {
